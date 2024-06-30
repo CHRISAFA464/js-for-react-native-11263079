ffd2d181f0927131f75de9b08608fcce88cf2ca6
CHRISAFA464 <cafatsiawu001@st.ug.edu.gh>
2024-06-29 22:40:25 -0800

    added formatArrayString

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -3,3 +3,9 @@ function processArray(arr) {
     return arr.map(num => num % 2 === 0 ? num * num : num * 3);
 }
 
+// Task 2
+function formatArrayStrings(strings, numbers) {
+    return strings.map((str, index) => numbers[index] % 2 === 0 ? str.toUpperCase() : str.toLowerCase());
+}
+
+module.exports = { processArray, formatArrayStrings };
