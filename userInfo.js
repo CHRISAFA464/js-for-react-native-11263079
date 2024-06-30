3ce1e8f7ab6cb7e60ab0d266658147e3ccafda7b
CHRISAFA464 <cafatsiawu001@st.ug.edu.gh>
2024-06-29 22:45:18 -0800

    added userInfofile

---
+++ userInfo.js
@@ -0,0 +1,11 @@
+const { formatArrayStrings } = require('./arrayManipulation.js');
+
+function createUserProfiles(names, modifiedNames) {
+    return names.map((name, index) => ({
+        originalName: name,
+        modifiedName: modifiedNames[index],
+        id: index + 1
+    }));
+}
+
+module.exports = { createUserProfiles };
